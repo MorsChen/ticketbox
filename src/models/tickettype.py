bc4d1e4ba7f61260a2cb064d59ff06e2c5c02733
MorsChen <MorsChenArtist@users.noreply.github.com>
2019-09-14 21:51:29 +0700

    add ticket type

--- src/models/tickettype.py
+++ src/models/tickettype.py
@@ -6,7 +6,11 @@ class Tickettype(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     event_id = db.Column(db.Integer,db.ForeignKey('events.id'))
-    name = db.Column(db.String(80))
+    name = db.Column(db.String(100))
     price = db.Column(db.Integer, default = 0)
-    quanity = db.Column(db.Integer, default = 0)
+    stockquanity = db.Column(db.Integer, default = 0)
+    restquantity = db.Column(db.Integer)
     ticket = db.relationship('Ticket', backref='tickettypes', cascade="all, delete-orphan", lazy='dynamic')
+    
+    def setrestquantity(self, quantity):
+        self.restquantity =  self.stockquanity - quantity
